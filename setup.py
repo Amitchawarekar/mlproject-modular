876fcbf9c36b817a39097e718b677c39f66529c0
Amit Chawarekar <amit.chawarekar@gmail.com>
2023-03-14 21:19:15 +0530

    Setup.py and requirements.txt added

---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages,setup
+from typing import List
+
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+    This function will return the list of requirements
+    '''
+
+    requirements =[]
+    with open(file_path) as file_obj:
+        requirements=file_obj.readlines()
+        requirements= [req.replace("\n","") for req in requirements ]
+        '''
+        This above list comprehension id for replacing \n with blank which get recorded when we use readline function
+        '''
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+        
+    return requirements 
+
+
+setup(
+name='mlproject-modular',
+version='0.0.1',
+author="Amit",
+author_email="amit.chawarekar22@spit.ac.in",
+packages=find_packages(),
+install_requires=get_requirements('requirements.txt')
+
+)
